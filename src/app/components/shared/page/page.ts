68729c65a641e890f8bf3ab5a341d9b5a5ab0cec
Wallace W <wallace_wolv@hotmail.com>
2025-12-05 17:07:57 -0300

    feat: Set up routing and create home screen component with template and styles

--- src/app/components/shared/page/page.ts
+++ src/app/components/shared/page/page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Menu } from "../menu/menu";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-page',
-  imports: [Menu],
+  imports: [Menu, RouterOutlet],
   templateUrl: './page.html',
   styleUrl: './page.css',
 })
