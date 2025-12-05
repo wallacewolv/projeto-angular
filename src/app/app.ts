ca855039eb6b66a583bc82b4b4654984fc134382
Wallace W <wallace_wolv@hotmail.com>
2025-12-05 16:46:30 -0300

    feat: Implement page component with layout and navigation structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Page } from './components/shared/page/page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Page],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('projeto-angular');
 }
---
+++ src/app/components/shared/page/page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page',
+  imports: [],
+  templateUrl: './page.html',
+  styleUrl: './page.css',
+})
+export class Page {}
